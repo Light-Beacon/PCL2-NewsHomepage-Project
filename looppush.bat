04ebb48100a8fb50b6f6cd536a0a761bf9e2ba00
Nattiden <2682635990@qq.com>
2023-01-05 23:12:15 +0800

    update

---
+++ looppush.bat
@@ -0,0 +1,14 @@
+@echo off
+cd .\PCL2-NewsHomepage
+chcp 65001
+:loop
+echo 正在推送...
+git push origin main
+if %ERRORLEVEL%==0 (
+    echo 已成功推送到远端仓库！
+    exit /b 0
+) else (
+    echo 推送到远端仓库失败，即将在 3 秒后重试...
+    timeout /t 3 /nobreak
+    goto :loop
+)
