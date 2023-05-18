df4a80f27b0d79c4b5113bdec509224ca9e951ac
Nattiden <2682645990@qq.com>
2023-05-18 10:08:10 +0800

    1.20 pre2

---
+++ push_project.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001
+:loop
+echo 正在推送...
+git push origin master
+if %ERRORLEVEL%==0 (
+    echo 已成功推送到远端仓库！
+    exit /b 0
+) else (
+    echo 推送到远端仓库失败，即将在 3 秒后重试...
+    timeout /t 3 /nobreak
+    goto :loop
+)
