64eda0141481bd8c70e9c1b542da4be413c5419b
Nattiden <2682645990@qq.com>
2023-05-23 23:57:13 +0800

    1.17 & 1.18 achives

--- push_all.bat
+++ push_all.bat
@@ -19,9 +19,13 @@ echo 正在推送工程文件...
 git push origin master
 if %ERRORLEVEL%==0 (
     echo 已成功推送工程文件到远端仓库！
-    exit /b 0
+    goto :done
 ) else (
     echo 推送到远端仓库失败，即将在 3 秒后重试...
     timeout /t 3 /nobreak
     goto :loop2
 )
+:done
+echo 推送完毕！将在 60 秒后退出...
+timeout /t 60 /nobreak
+exit /b 0
